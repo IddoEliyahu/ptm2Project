ca206389988684cfee3e33f870fb982a16a1bb92
Iddo <iddoel10@gmail.com>
2021-06-30 00:39:57 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -15,21 +15,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
-//        primaryStage.setTitle("ListView Experiment 1");
-//
-//        ListView listView = new ListView();
-//
-//        listView.getItems().add("Item 1");
-//        listView.getItems().add("Item 2");
-//        listView.getItems().add("Item 3");
-//
-//
-//
-//        HBox hbox = new HBox(listView);
-//
-//        Scene scene = new Scene(hbox, 300, 120);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
     }
 
 
